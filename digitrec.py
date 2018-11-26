28c6f18b5c1d813e2617434db4f195bdde50eac5
NiallD565 <G00338932@gmit.ie>
2018-11-26 17:44:07 +0000

    digitrec up to 95% accuracy

--- digitrec.py
+++ digitrec.py
@@ -1,42 +1,14 @@
-# Adapted from https://docs.python.org/3/library/gzip.html
-
 # For unzipping the file within the script.
 import gzip
 with gzip.open('data/t10k-images-idx3-ubyte.gz', 'rb') as f:
     file_content_images = f.read()
     
-# Adapted from: https://docs.python.org/2/library/gzip.html
 # For unzipping the file within the script.
 with gzip.open('data/t10k-labels-idx1-ubyte.gz', 'rb') as f:
     file_content_labels = f.read()
 	
-	
-
 import numpy as np
-# Import keras.
-import keras as kr
-# Importing the required Keras modules containing model and layers
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D, Activation
-
 image = ~np.array(list(file_content_images[16:800])).reshape(28,28).astype(np.uint8)
-
-# Start a neural network, building it by layers.
-model = kr.models.Sequential()
-
-# Add a hidden layer with 2000 neurons and an input layer with 784.
-model.add(kr.layers.Dense(units=1000, activation='relu', input_dim=784))
-# Add a hidden layer with 1000 neurons and an input layer with 784.
-model.add(kr.layers.Dense(units=750, activation='relu', input_dim=784))
-# Add a hidden layer with 1000 neurons and an input layer with 784.
-model.add(kr.layers.Dense(units=250, activation='relu', input_dim=784))
-# Dropout drops random biases within the network as it trains
-model.add(Dropout(0.2))
-# Add a three neuron output layer.
-model.add(kr.layers.Dense(units=10, activation='softmax'))
-
-# Build the graph.
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
     train_img = f.read()
@@ -44,10 +16,8 @@ with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
 with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
     train_lbl = f.read()
     
-train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8)
+train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8)/255.0
 train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
-
-inputs = train_img.reshape(60000, 784)
 
 # For encoding categorical variables.
 import sklearn.preprocessing as pre
@@ -56,5 +26,31 @@ encoder = pre.LabelBinarizer()
 encoder.fit(train_lbl)
 outputs = encoder.transform(train_lbl)
 
+outputs[0]
+
+inputs = train_img.reshape(60000, 784)
+
+# ------- MODEL -------
+# Import keras.
+import keras as kr
+# Import Tensorflow
+import tensorflow as tf
+# Importing the required Keras modules containing model and layers
+from keras.models import Sequential
+# Start a neural network, building it by layers.
+model = kr.models.Sequential()
+# model.add(kr.layers.Flatten())
+# Add a hidden layer with 300 neurons and an input layer with 784.
+model.add(kr.layers.Dense(units=300, activation='relu'))
+# Add a hidden layer with 150 neurons and an input layer with 784.
+model.add(kr.layers.Dense(units=150, activation='relu'))
+# Add a hidden layer with 50 neurons and an input layer with 784.
+model.add(kr.layers.Dense(units=50, activation='relu'))
+# Add a three neuron output layer.
+model.add(kr.layers.Dense(units=10, activation='softmax'))
+
+# Build the graph.
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
 # Don't run this unless you really want to
-model.fit(inputs, outputs, epochs=10, batch_size=15)
+model.fit(inputs, outputs, epochs=5, batch_size=15)
